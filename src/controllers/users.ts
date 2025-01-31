7f7450fa9243772e2ceafdad94874f26012f5c91
Bluette1 <marylene.sawyer@gmail.com>
2025-01-31 21:30:18 +0200

    chore: Run Linter
    - Run linter
    
    Issue #2

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,11 +1,12 @@
-import { Request, Response } from "express";
-import { normalizeEmail } from "validator";
-import User, { IUser } from "../models/user";
-const nodemailer = require("nodemailer");
+import { Request, Response } from 'express';
+import { normalizeEmail } from 'validator';
+import User, { IUser } from '../models/user';
+
+const nodemailer = require('nodemailer');
 
 // Configure email transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: 'gmail',
   auth: {
     user: `${process.env.USER}`,
     pass: `${process.env.PASSWORD}`,
@@ -19,7 +20,7 @@ class UsersController {
       const users = await User.find();
       res.status(200).json(users);
     } catch (error) {
-      res.status(500).json({ message: "Server error" });
+      res.status(500).json({ message: 'Server error' });
     }
   }
 
@@ -31,16 +32,18 @@ class UsersController {
       if (user) {
         res.status(200).json(user);
       } else {
-        res.status(404).json({ message: "User not found" });
+        res.status(404).json({ message: 'User not found' });
       }
     } catch (error) {
-      res.status(500).json({ message: "Server error" });
+      res.status(500).json({ message: 'Server error' });
     }
   }
 
   // Create a new user
   public async createUser(req: Request, res: Response): Promise<void> {
-    const { name, email, password, role } = req.body;
+    const {
+      name, email, password, role,
+    } = req.body;
 
     try {
       const newUser = new User({
@@ -52,9 +55,9 @@ class UsersController {
       const currentUserRole = req.user?.role;
 
       // Prevent normal users from creating admin users
-      if (role === "admin" && currentUserRole !== "admin") {
+      if (role === 'admin' && currentUserRole !== 'admin') {
         res.status(403).json({
-          message: "Only admins can create users with the admin role",
+          message: 'Only admins can create users with the admin role',
         });
         return; // Ensure no further code executes
       }
@@ -65,7 +68,7 @@ class UsersController {
       const mailOptions = {
         from: `${process.env.USER}`,
         to: savedUser.email,
-        subject: "Welcome to Dish Discovery! Registration Successful",
+        subject: 'Welcome to Dish Discovery! Registration Successful',
         html: `
         <h1>Welcome!</h1>
         <p>Thank you for registering with our service.</p>
@@ -82,7 +85,7 @@ class UsersController {
         email: savedUser.email,
       });
     } catch (error) {
-      if (JSON.stringify(error).includes("duplicate key")) {
+      if (JSON.stringify(error).includes('duplicate key')) {
         const existingUser = await User.findOne({
           email: normalizeEmail(email),
         });
@@ -93,7 +96,7 @@ class UsersController {
           email: existingUser?.email,
         });
       } else {
-        res.status(500).json({ message: "Server error", error });
+        res.status(500).json({ message: 'Server error', error });
       }
     }
   }
@@ -102,12 +105,14 @@ class UsersController {
   public async updateUser(req: Request, res: Response): Promise<void> {
     try {
       const { id } = req.params;
-      const { name, email, password, role } = req.body;
+      const {
+        name, email, password, role,
+      } = req.body;
       const currentUserRole = req.user?.role;
 
       // Prevent normal users from changing roles
-      if (role && currentUserRole !== "admin") {
-        res.status(403).json({ message: "Only admins can change user roles" });
+      if (role && currentUserRole !== 'admin') {
+        res.status(403).json({ message: 'Only admins can change user roles' });
         return; // Ensure no further code executes
       }
 
@@ -129,10 +134,10 @@ class UsersController {
           email: updatedUser.email,
         });
       } else {
-        res.status(404).json({ message: "User not found" });
+        res.status(404).json({ message: 'User not found' });
       }
     } catch (error) {
-      res.status(500).json({ message: "Server error" });
+      res.status(500).json({ message: 'Server error' });
     }
   }
 
@@ -144,10 +149,10 @@ class UsersController {
       if (deletedUser) {
         res.status(204).send();
       } else {
-        res.status(404).json({ message: "User not found" });
+        res.status(404).json({ message: 'User not found' });
       }
     } catch (error) {
-      res.status(500).json({ message: "Server error" });
+      res.status(500).json({ message: 'Server error' });
     }
   }
 }
